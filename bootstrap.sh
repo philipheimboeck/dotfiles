ab3da61060855eacffe90ff18a3b13d82e2fe841
Philip Heimböck <philipheimboeck@gmail.com>
2018-01-10 08:50:20 +0100

    Adding fish config

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,5 +5,9 @@ pwd
 
 echo 'Creating Symlinks'
 
+echo '~/.config/fish/config.fish'
+ln -s (pwd)/fish/config.fish ~/.config/fish/config.fish
+
+echo '~/.gitconfig'
 ln -s (pwd)/git/.gitconfig ~/.gitconfig
 
