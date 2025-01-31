e338382b3081edea0fdd3b5aae4d8e6cff0b2516
Philip Heimböck <philip.heimboeck@abacus.ch>
2025-01-31 09:07:19 +0100

    add telescope search to file tree

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -410,6 +410,28 @@ require("lazy").setup({
         config = function()
             vim.keymap.set('n', '<leader>fr', ':Neotree reveal<CR>', { desc = 'Neotree reveal' })
 
+            local function getTelescopeOpts(state, path)
+                return {
+                    cwd = path,
+                    search_dirs = { path },
+                    attach_mappings = function(prompt_bufnr, map)
+                        local actions = require "telescope.actions"
+                        actions.select_default:replace(function()
+                            actions.close(prompt_bufnr)
+                            local action_state = require "telescope.actions.state"
+                            local selection = action_state.get_selected_entry()
+                            local filename = selection.filename
+                            if (filename == nil) then
+                                filename = selection[1]
+                            end
+                            -- any way to open the file without triggering auto-close event of neo-tree?
+                            require("neo-tree.sources.filesystem").navigate(state, state.path, filename)
+                        end)
+                        return true
+                    end
+                }
+            end
+
             require('neo-tree').setup({
                 filesystem = {
                     filtered_items = {
@@ -417,6 +439,24 @@ require("lazy").setup({
                     },
                     follow_current_file = { enabled = true },
                     use_libuv_file_watcher = true,
+                    window = {
+                        mappings = {
+                            ["g"] = "telescope_grep",
+                            ["f"] = "telescope_find",
+                        }
+                    }
+                },
+                commands = {
+                    telescope_find = function(state)
+                        local node = state.tree:get_node()
+                        local path = node:get_id()
+                        require('telescope.builtin').find_files(getTelescopeOpts(state, path))
+                    end,
+                    telescope_grep = function(state)
+                        local node = state.tree:get_node()
+                        local path = node:get_id()
+                        require('telescope.builtin').live_grep(getTelescopeOpts(state, path))
+                    end,
                 },
                 buffers = { follow_current_file = { enabled = true } }
             })
@@ -428,7 +468,11 @@ require("lazy").setup({
         event = 'VeryLazy',
         version = '2.*',
         config = function()
-            require 'window-picker'.setup()
+            require 'window-picker'.setup(
+                {
+                    hint = 'floating-big-letter'
+                }
+            )
         end,
     },
     {
